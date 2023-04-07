3e7f927b9ab62084c7e1ad2baef7644d659364eb
tharindu432 <tharinduchaturanga432@gmail.com>
2023-04-07 19:20:34 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+#variables
+rent= 1220
+gas= 67.89
+groceries=789
+total= rent+gas+groceries
+print(total)
+print(rent)
+sum= 5/4+2
+print(sum)
+
+#string
+myName= "tharindu"
+print(myName)
+x=myName[0]
+print(x)
+
+print(myName[0:3])
+
+#list
+item= ["breed","pasta","fruits"];
+print(item);
+print(item[0]);
+
+
+
